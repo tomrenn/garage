71287bdeae122970ae1f534477d03b922c2ea095
Tom Renn <tomrenn@gmail.com>
2018-02-17 13:27:52 -0800

    Add referer query param

--- garage/app.py
+++ garage/app.py
@@ -1,14 +1,18 @@
 import socket, threading
 from time import sleep
-from flask import Flask, request
+from flask import Flask, request, redirect
 from gpiozero import OutputDevice
 from gpiozero.pins.mock import MockPin
 app = Flask(__name__)
 
 ip = socket.gethostbyname(socket.gethostname())
+host = socket.gethostname()
+print(ip)
+print(host)
 
 pin = 4
-if (ip != '192.168.29.29'):
+if (host != 'raspberrypi'):
+    print('USING MOCK PIN')
     pin = MockPin(pin)
 
 relay = OutputDevice(pin)
@@ -20,14 +24,20 @@ def toggleRelay():
 
 @app.route('/garage', methods=['GET', 'POST'])
 def garage():
+    redirect_addr = request.args.get('referer')
+
     if request.method == 'POST':
         print('POST')
+        print(request.headers)
         t = threading.Thread(target=toggleRelay)
         t.daemon = True
         t.start()
     else:
         print('GET')
 
+
+    if redirect_addr:
+        return redirect(redirect_addr + "#success")
     return 'OK'
 
 app.run()
